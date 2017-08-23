f8d8e1e933da845fdff631e7c85bf32ba2de4102
Sam Frances <sam@samfrances.uk>
2017-08-23 21:56:37 +0100

    Experimental redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,23 @@
 import "core-js";
+import * as redux from 'redux';
+
+interface MyAction extends redux.Action {
+    type: "hello";
+}
+
+interface MyState {
+    counter: number;
+}
+
+interface MyStore extends redux.Store<MyState> {
+    dispatch<A extends MyAction>(action: A): A;
+}
+
+const reducer = function(state: MyState, action: MyAction) {
+    return {counter: state.counter + 1};
+}
+
+const store: MyStore = redux.createStore(reducer, {counter: 5});
+
+store.dispatch({ type: "hello" })
+console.log(store.getState())
