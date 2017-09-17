7b8b748fd2a2687a0cc04fe804b8f984d2ecef76
Sam Frances <sam@samfrances.uk>
2017-09-17 18:44:20 +0100

    Basic state template

--- src/state.ts
+++ src/state.ts
@@ -1,6 +1,10 @@
 /* State types */
 
+import * as redux from "redux";
+import thunk from 'redux-thunk';
+
 import { Post, Comment } from "./interfaces";
+import { ActionTypesSynch, ActionTypesAsync } from "./actions";
 
 interface EntityIndex<T> {
     byId: {
@@ -16,22 +20,40 @@ export interface ApplicationState {
     };
 }
 
-/* Reducers */
-
-
+export interface ApplicationStore {
+    dispatch(action: ActionTypesSynch): ActionTypesSynch;
+    dispatch(action: ActionTypesAsync): Promise<ActionTypesAsync>;
+    getState(): ApplicationState;
+    subscribe(listener: () => void): redux.Unsubscribe;
+    replaceReducer(nextReducer: redux.Reducer<ApplicationState>): void;
+}
 
 /* Initial state */
 
-const initialState: ApplicationState = {
-    entities: {
-        posts: {
-            byId: {},
-            allIds: []
-        },
-        comments: {
-            byId: {},
-            allIds: []
+const getInitialState: () => ApplicationState =
+    () => ({
+        entities: {
+            posts: {
+                byId: {},
+                allIds: []
+            },
+            comments: {
+                byId: {},
+                allIds: []
+            }
         }
-    }
+    });
+
+/* Reducers */
+
+function myReducer(state: ApplicationState, action: ActionTypesSynch): ApplicationState {
+    return state;
 }
 
+function storeFactory (): ApplicationStore {
+    return redux.createStore(
+        myReducer,
+        getInitialState(),
+        redux.applyMiddleware(thunk)
+    );
+}
